d92a0c6cd3ac8ef96d53b5844c01d4754c03a015
BÜNYAMİN TÜNÇ <192803021@ogr.cbu.edu.tr>
2021-01-22 22:33:48 +0300

    Proje dosyası ekle.

---
+++ MyList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> listem = new MyList<string>();
+            listem.Add("NAME1");
+            Console.WriteLine(listem.Lenght());
+            listem.Add("Name2");
+            Console.WriteLine(listem.Lenght());
+        }
+    }
+}
